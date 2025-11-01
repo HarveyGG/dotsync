f833b326dd1b12a37f22b1abe0991b8ab3e05b08
Test User <test@example.com>
2025-11-01 09:37:31 -0700

    Bump version to v1.0.0

--- dotsync/info.py
+++ dotsync/info.py
@@ -1,7 +1,7 @@
 from os.path import expanduser
 import socket
 
-__version__ = '2.2.9'
+__version__ = '1.0.0'
 __author__ = 'Harvey'
 __author_email__ = 'harvey.wanghy@gmail.com'
 __url__ = 'https://github.com/HarveyGG/dotsync'
